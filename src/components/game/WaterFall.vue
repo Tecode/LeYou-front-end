20a73c66d1a68541b28ae63e50b3a76d83762f24
Tecode <283731869@qq.com>
2017-08-26 10:50:42 +0800

    '完成基本布局'

--- src/components/game/WaterFall.vue
+++ src/components/game/WaterFall.vue
@@ -29,9 +29,35 @@
     .water_fall_container {
         margin: 50px auto 40px auto;
         transition: all .3s;
-        .box {
+        position: relative;
+        .water_fall_container_box {
             width: 200px;
             background-color: #3d1e37;
+            background-size: cover;
+            box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.22);
+            transition: all .3s;
+            &:hover{
+                box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.3);
+            }
+        }
+        .water_fall_container_box_link{
+            display: block;
+            height: 100%;
+        }
+        .water_fall_container_link {
+            background-color: #fed919;
+            display: block;
+            position: absolute;
+            width: 100%;
+            bottom: 0;
+            left: 0;
+            color: #223343;
+            transition: all .3s;
+            padding: 8px 0;
+            text-decoration: none;
+            &:hover{
+                text-decoration: underline;
+            }
         }
     }
     .add_more_button{
--- src/components/game/waterFall.js
+++ src/components/game/waterFall.js
@@ -2,13 +2,18 @@ import Bricks from 'bricks.js';
 
 const gregenerateat = () => {
 	const container = document.querySelector('.water_fall_container');
+	const hightArr = [320, 340, 360];
 	// insert 100 divs with same width, but random height
 	for (let i = 0; i < 20; i++) {
 		const div = document.createElement('div');
-		div.setAttribute("class", "box");
-		div.style.width = '200px';
-		div.style.height = Math.round(Math.random() * 400) + 'px';
-		container.appendChild(div)
+		div.setAttribute("class", "water_fall_container_box");
+		div.style.backgroundImage = "url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1503722945921&di=cc4cfb46bb9b08f3f20a481b7de8ac16&imgtype=0&src=http%3A%2F%2Fimg4.cache.netease.com%2Fphoto%2F0031%2F2017-03-03%2FCEITI5KQ4UUJ0031.jpg)";
+		div.style.height = `${hightArr[Math.ceil(Math.random()*3) - 1]}px`;
+		div.innerHTML= '<a href="#" target="_blank" class="water_fall_container_box_link">' +
+		'<h3></h3>' +
+		'<a href="#" target="_blank" class="water_fall_container_link text-center">开始游戏</a>' +
+		'</a>';
+		container.appendChild(div);
 	}
 	const instance = Bricks({
 		container: '.water_fall_container',
